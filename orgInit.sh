219136be2e927ea4476b3682bf3f1326abb70c6e
Sadhana Nandakumar <39506194+snandakumarops@users.noreply.github.com>
2025-12-12 10:01:06 -0500

    Update orgInit.sh
    updated scratch def

--- orgInit.sh
+++ orgInit.sh
@@ -2,7 +2,7 @@
 
 # Create scratch org (use default dev hub or specify with -v flag)
 #sf org create scratch --definition-file config/project-scratch-def.json --duration-days 30 --wait 60 --target-dev-hub alm_demo_hub_org_2 --set-default --alias cee-scratch-4
-sf org create scratch --definition-file config/project-scratch-def.json --duration-days 30 --wait 60
+sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p flow -e platformfreetrial.demo
 
 # Deploy source (excluding Wave apps and dashboards - they need permissions first)
 sf project deploy start
